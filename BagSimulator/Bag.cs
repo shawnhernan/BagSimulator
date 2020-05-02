baecb6ab636d07081ae15a7c8bc506b1baca042e
Shawn Hernan <shawnhernan@hotmail.com>
2020-05-02 14:53:18 -0700

    Add project files.

---
+++ BagSimulator/Bag.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BagSimulator
+{
+
+    class Bag
+    {
+        public uint Apples { get; set; }
+        public uint Oranges { get; set; }
+        private List<string> fruits = new List<string>();
+        public Bag(uint Apples, uint Oranges)
+        {
+
+            for (uint i = 0; i < Apples; i++)
+            {
+                fruits.Add("apple");
+            }
+
+            for (uint i = Apples; i < (Oranges + Apples); i++)
+            {
+                fruits.Add("orange");
+
+            }
+        }
+
+        public int getLength()
+        {
+            if (fruits.Count > 2) throw new Exception("too big");
+            return fruits.Count;
+        }
+
+        public void Print()
+        {
+            foreach (string f in fruits)
+            {
+                //Console.Write(f);
+                //Console.Write(" "); 
+            }
+            //Console.WriteLine(); 
+        }
+
+        public void Shake()
+        {
+            if (fruits.Count > 2) throw new Exception("too big");
+            Random rnd = new Random();
+            int flip = 0;
+            //int count = 0; 
+            //string[] tempStrings = new string[2];
+
+            //flip = rnd.Next(0, fruits.Length);
+
+            if (flip == 1)
+            {
+                //fruits[0] = tempStrings[1]; 
+                //fruits[1] = tempStrings[0]; 
+            }
+
+        }
+
+        public string[] BuildNewArrray(string[] sourceArr, string[] targetArr, int exclude)
+        {
+            if (exclude == 0)
+            {
+                targetArr[0] = sourceArr[1];
+            }
+            else
+            {
+                targetArr[0] = sourceArr[0];
+            }
+            return targetArr;
+        }
+
+        public string Draw()
+        {
+            if (fruits.Count > 2) throw new Exception("too big");
+            if (fruits.Count < 1) throw new Exception("too small"); 
+
+            string[] theFruit = new string[1];
+            Random rnd = new Random();
+            int res = rnd.Next(0, fruits.Count);
+            fruits.CopyTo(res, theFruit, 0, 1);
+            fruits.RemoveAt(res);
+            fruits.Capacity = 1;
+            fruits.TrimExcess();
+            return theFruit[0];
+        }
+    }
+
+}
---
+++ BagSimulator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq; 
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace BagSimulator
+{
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bertrand's Box");
+
+            #region DeclareVariables
+            Bag[] Bags = new Bag[3];
+            Bag Bag0 = new Bag(2, 0);   // two apples
+            Bag Bag1 = new Bag(0, 2);   // two oranges
+            Bag Bag2 = new Bag(1, 1);   // one of each 
+            Random rnd = new Random();
+            string firstDraw, secondDraw;
+            int randomBag;
+
+            randomBag = rnd.Next(0, Bags.Length);
+            Bag theBag = Bags[randomBag];
+
+            int firstAppleDraws = 0;
+            int secondOrangeDraws = 0;
+            #endregion
+            for (uint i = 0; i < 1000000; i++)
+            {
+                //create some bags, and shake them up 
+                Bag0 = new Bag(2, 0); Bag1 = new Bag(0, 2); Bag2 = new Bag(1, 1); 
+                Bags[0] = Bag0; Bags[1] = Bag1; Bags[2] = Bag2;
+                Bag0.Shake(); Bag1.Shake(); Bag2.Shake();
+
+                //choose a bag at random
+                randomBag = rnd.Next(0, Bags.Length);
+                theBag = Bags[randomBag];
+
+                //draw two pieces of fruit from the randonly chosen bag 
+                firstDraw = theBag.Draw(); 
+                secondDraw = theBag.Draw(); 
+               
+                if (firstDraw.Equals("apple"))
+                {
+                    firstAppleDraws++;
+                    if (secondDraw.Equals("orange")) secondOrangeDraws++; // if you drew an apple first, how many times will the second draw be an orange
+                }
+            }
+            float ratio = (float) secondOrangeDraws / (float) firstAppleDraws; 
+            Console.WriteLine("first draws {0}, secondDraws {1}, percentage {2}", firstAppleDraws, secondOrangeDraws, ratio);
+        }
+    }
+}
